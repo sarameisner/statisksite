cdd567b695e24211a6e985bbe461894ff8ce8714
Sara <sala0002@stud.kea.dk>
2024-01-31 20:29:34 +0100

    sidste rettelser

---
+++ size.js
@@ -0,0 +1,5 @@
+function selectSize() {
+  var selectedSize = document.getElementById("sizeSelect").value;
+}
+// document.getElementById henter id'et sizeSelect. Id'et er nemlig min "dropdown menu".
+// .value bruges til at hente de forskellige størrelser - altså værdien
